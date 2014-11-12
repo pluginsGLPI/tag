80ae239a28fdb5f890b292a750d5388c29911b16
Emmanuel Haguet <ehaguet@teclib.com>
2014-11-12 16:02:12 +0100

    Supression de fichiers inutiles

--- search3.sql
+++
@@ -1,4 +0,0 @@
-SELECT *, GROUP_CONCAT(`glpi_plugin_tag_etiquetteitems`.`plugin_tag_etiquettes_id`) AS texte
-FROM `glpi_tickets`
-LEFT JOIN `glpi_plugin_tag_etiquetteitems` ON (`glpi_tickets`.`id` = `glpi_plugin_tag_etiquetteitems`.`glpiobject_id`) 
-GROUP BY `glpi_tickets`.id
--- search4.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-#, GROUP_CONCAT(`glpi_plugin_tag_etiquetteitems`.`plugin_tag_etiquettes_id`) AS texte
-, GROUP_CONCAT(`glpi_plugin_tag_etiquetteitems`.`plugin_tag_etiquettes_id`) AS texte
-, GROUP_CONCAT(`glpi_plugin_tag_etiquettes`.`name`) AS name
-FROM `glpi_tickets`
-LEFT JOIN `glpi_plugin_tag_etiquetteitems` ON (`glpi_tickets`.`id` = `glpi_plugin_tag_etiquetteitems`.`glpiobject_id`) #3,2
-LEFT JOIN `glpi_plugin_tag_etiquettes` ON (`glpi_plugin_tag_etiquettes`.`id` = `glpi_plugin_tag_etiquetteitems`.`plugin_tag_etiquettes_id`) #name
-GROUP BY `glpi_tickets`.id
