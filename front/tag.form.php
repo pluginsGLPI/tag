10c84099be494784f6c1ad3c095429045937293b
Emmanuel Haguet <ehaguet@teclib.com>
2015-01-05 14:26:38 +0100

    Check unicity before add

--- front/tag.form.php
+++ front/tag.form.php
@@ -10,7 +10,15 @@ $dropdown = new PluginTagTag();
 if (isset($_POST['add'])) {
    $item = new PluginTagTagItem();
    //$_REQUEST['itemtype'] = strtolower($_REQUEST['itemtype']);
-   $item->add($_REQUEST);
+   
+   // Check unicity :
+   $found = $item->find('plugin_tag_tags_id = '. $_REQUEST['plugin_tag_tags_id'] .'
+         AND items_id = ' . $_REQUEST['items_id'].'
+         AND itemtype = "' . $_REQUEST['itemtype'].'"');
+   
+   if (count($found) == 0) {
+      $item->add($_REQUEST);
+   }
 }
 
 include (GLPI_ROOT . "/front/dropdown.common.form.php");
