8632f94833fb4bdb703ab4d6d9fc3202b214fc90
Emmanuel Haguet <ehaguet@teclib.com>
2015-01-07 09:22:56 +0100

    don't charge JS in */front/_itemtype_.form.php without parameters

--- setup.php
+++ setup.php
@@ -49,7 +49,7 @@ function plugin_init_tag() {
    if (preg_match_all("/.*\/(.*)\.form\.php/", $_SERVER['REQUEST_URI'], $matches) !== false) {
 
       if (strpos($_SERVER['REQUEST_URI'], "/front/dropdown.php") === false &&
-         strpos($_SERVER['REQUEST_URI'], ".form.php") !== false) {
+         strpos($_SERVER['REQUEST_URI'], ".form.php?") !== false) {
          $PLUGIN_HOOKS['add_javascript']['tag'] = array('lib/chosen/chosen.native.js', 
                                                         'js/show_tags.js');
       }
