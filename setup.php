0dd3d73ef3da96159c30846c9a627a56166dbf07
Emmanuel Haguet <ehaguet@teclib.com>
2015-01-07 09:53:21 +0100

    Don't add JS in Computer form creation (id=-1)

--- setup.php
+++ setup.php
@@ -50,6 +50,7 @@ function plugin_init_tag() {
 
       if (strpos($_SERVER['REQUEST_URI'], "/front/dropdown.php") === false && 
          strpos($_SERVER['REQUEST_URI'], ".form.php?") !== false && 
+         strpos($_SERVER['REQUEST_URI'], "id=-1") === false && //for Computer
          strpos($_SERVER['REQUEST_URI'], "plugins/tag/front/tag.form.php") === false) {
          $PLUGIN_HOOKS['add_javascript']['tag'] = array('lib/chosen/chosen.native.js', 
                                                         'js/show_tags.js');
