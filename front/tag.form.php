1adec1a7b4768cd22bda09df2766107f4268a092
Emmanuel Haguet <ehaguet@teclib.com>
2015-01-08 10:07:39 +0100

    Tag : Name mandatory -> best check

--- front/tag.form.php
+++ front/tag.form.php
@@ -8,7 +8,7 @@ Plugin::load('tag', true);
 $dropdown = new PluginTagTag();
 
 if (isset($_POST['add']) || isset($_REQUEST['update'])) {
-   if (isset($_REQUEST['name']) && empty($_REQUEST['name'])) {
+   if (isset($_REQUEST['name']) && empty(trim($_REQUEST['name']))) {
       Session::addMessageAfterRedirect(__('Not saved : Name is a required field.', 'tag'), false, ERROR);
       Html::back();
    }
