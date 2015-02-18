bd9a2cbff7856ee90bf25f8491cbda29fdb88832
Emmanuel Haguet <ehaguet@teclib.com>
2015-02-18 10:52:22 +0100

    Add a test plugin is actived on /front/tag.php

--- front/tag.php
+++ front/tag.php
@@ -3,5 +3,10 @@ include ('../../../inc/includes.php');
 
 Plugin::load('tag', true);
 
-$dropdown = new PluginTagTag();
-include (GLPI_ROOT . "/front/dropdown.common.php");
+$plugin = new Plugin();
+if ($plugin->isInstalled("tag") && $plugin->isActivated("tag")) {
+   $dropdown = new PluginTagTag();
+   include (GLPI_ROOT . "/front/dropdown.common.php");
+}
+
+Html::displayNotFoundError();
