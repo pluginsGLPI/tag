067dad4be36dd33991a2671d6cd0812ce1028893
Emmanuel Haguet <ehaguet@teclib.com>
2015-10-26 16:32:14 +0100

    Minor code organisation in front/tag.form.php

--- front/tag.form.php
+++ front/tag.form.php
@@ -10,30 +10,30 @@ if (! $plugin->isInstalled("tag") || ! $plugin->isActivated("tag")) {
    Html::displayNotFoundError();
 }
 
-$dropdown = new PluginTagTag();
-
 if (isset($_POST['add']) || isset($_REQUEST['update'])) {
    if (isset($_REQUEST['name']) && trim($_REQUEST['name']) == false) {
       Session::addMessageAfterRedirect(__('Not saved : Name is a required field.', 'tag'), false, ERROR);
       Html::back();
    }
-}
 
-if (isset($_POST['add'])) {
-   $item = new PluginTagTagItem();
-   
-   // Check unicity :
-   if (isset($_REQUEST['plugin_tag_tags_id'])) {
-      $found = $item->find('plugin_tag_tags_id = '. $_REQUEST['plugin_tag_tags_id'] .'
-            AND items_id = ' . $_REQUEST['items_id'].'
-            AND itemtype = "' . $_REQUEST['itemtype'].'"');
+   if (isset($_POST['add'])) {
+      $item = new PluginTagTagItem();
       
-      if (count($found) == 0) {
+      // Check unicity :
+      if (isset($_REQUEST['plugin_tag_tags_id'])) {
+         $found = $item->find('plugin_tag_tags_id = '. $_REQUEST['plugin_tag_tags_id'] .'
+               AND items_id = ' . $_REQUEST['items_id'].'
+               AND itemtype = "' . $_REQUEST['itemtype'].'"');
+         
+         if (count($found) == 0) {
+            $item->add($_REQUEST);
+         }
+      } else {
          $item->add($_REQUEST);
       }
-   } else {
-      $item->add($_REQUEST);
    }
 }
+
+$dropdown = new PluginTagTag();
 
 include (GLPI_ROOT . "/front/dropdown.common.form.php");
