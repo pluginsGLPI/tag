6c1c495afb442dfb78dfd5f4eeba962bc28aac6b
Ludovic Dupont <ludovic.dupont@infotel.com>
2015-11-04 11:42:37 +0100

    Bug with the Tag column in the ticket list
    fix #6

--- hook.php
+++ hook.php
@@ -91,26 +91,27 @@ function plugin_tag_giveItem($type, $field, $data, $num, $linkfield = "") {
    
    switch ($field) {
       case "10500": //Note : can declare a const for "10500"
-
          $out = '<div id="s2id_tag_select" class="select2-container select2-container-multi chosen-select-no-results" style="width: 100%;">
-                  <ul class="select2-choices">';
+                 <ul class="select2-choices">';
          $separator = '';
-
          $plugintagtag = new PluginTagTag();
+         
+         foreach ($data[$num] as $tag) {
+            if (isset($tag['id']) && isset($tag['name'])) {
+               $id    = $tag['id'];
+               $name  = $tag['name'];
+               
+               $plugintagtag->getFromDB($id);
+               $color = $plugintagtag->fields["color"];
 
-         foreach (explode(Search::LONGSEP, $data['raw']["ITEM_$num"]) as $tag) {
-            $style = "";
+               $style = "";
+               if (!empty($color)) {
+                  $style .= "border-width:2px; border-color:$color;";
+               }
 
-            list($name, $id) = explode(Search::SHORTSEP, $tag);
-            
-            $plugintagtag->getFromDB($id);
-            $color = $plugintagtag->fields["color"];
-            if (! empty($color)) {
-               $style .= "border-width:2px; border-color:$color;";
+               $out .= '<li class="select2-search-choice" style="padding-left:5px;'.$style.'">'.$separator.$name.'</li>';
+               $separator = '<span style="display:none">, </span>'; //For export (CSV, PDF) of GLPI core
             }
-
-            $out .= '<li class="select2-search-choice" style="padding-left:5px;'.$style.'">'.$separator.$name.'</li>';
-            $separator = '<span style="display:none">, </span>'; //For export (CSV, PDF) of GLPI core
          }
          $out .= '</ul></div>';
          return $out;
