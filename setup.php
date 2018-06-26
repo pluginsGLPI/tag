84ea6780ac8810aad77f67af22709fc5f8a9d37a
Cédric Anne <cedric.anne@gmail.com>
2018-06-26 11:24:10 +0200

    Add 2.2.0 version definition

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_TAG_VERSION', '2.1.2');
+define ('PLUGIN_TAG_VERSION', '2.2.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_TAG_MIN_GLPI", "9.3");
