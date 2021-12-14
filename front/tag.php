9927b489a3c19731b50ed7489d381c9870a3116f
Cédric Anne <cedric.anne@gmail.com>
2021-12-14 22:23:40 +0100

    Fix licence headers

--- front/tag.php
+++ front/tag.php
@@ -1,4 +1,32 @@
 <?php
+
+/**
+ * -------------------------------------------------------------------------
+ * Tag plugin for GLPI
+ * Copyright (C) 2003-2021 by the Tag Development Team.
+ *
+ * https://github.com/pluginsGLPI/tag
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Tag.
+ *
+ * Tag is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tag is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tag. If not, see <http://www.gnu.org/licenses/>.
+ *  --------------------------------------------------------------------------
+ */
+
 include ('../../../inc/includes.php');
 
 $plugin = new Plugin();
