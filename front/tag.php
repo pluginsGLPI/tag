5323f72faaa03fd29b9b2390aa77c2a291990261
Cédric Anne <cedric.anne@gmail.com>
2022-07-25 16:24:20 +0200

    Prevent useless DB queries on plugin state checks

--- front/tag.php
+++ front/tag.php
@@ -30,9 +30,7 @@
 
 include ('../../../inc/includes.php');
 
-$plugin = new Plugin();
-if ($plugin->isInstalled("tag")
-    && $plugin->isActivated("tag")) {
+if (Plugin::isPluginActive("tag")) {
 
    $dropdown = new PluginTagTag();
    include (GLPI_ROOT . "/front/dropdown.common.php");
