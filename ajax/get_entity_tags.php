0b9cea164b3b720e2ffe345d065ce81358690d8c
Johan Cwiklinski <johan@x-tnd.be>
2023-11-08 09:25:17 +0100

    phpcbf

--- ajax/get_entity_tags.php
+++ ajax/get_entity_tags.php
@@ -28,14 +28,14 @@
  * -------------------------------------------------------------------------
  */
 
-include ("../../../inc/includes.php");
+include("../../../inc/includes.php");
 
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 Session::checkLoginUser();
 
 if (!isset($_REQUEST['name'])) {
-   exit;
+    exit;
 }
 
 echo PluginTagTag::getTagForEntityName($_REQUEST['name']);
--- inc/taginjection.class.php
+++ inc/taginjection.class.php
@@ -28,57 +28,63 @@
  * -------------------------------------------------------------------------
  */
 
-class PluginTagTagInjection extends PluginTagTag
-      implements PluginDatainjectionInjectionInterface {
+class PluginTagTagInjection extends PluginTagTag implements PluginDatainjectionInjectionInterface
+{
+    static function getTable($classname = null)
+    {
+        $parenttype = get_parent_class();
+        return $parenttype::getTable();
+    }
 
-   static function getTable($classname = null) {
-      $parenttype = get_parent_class();
-      return $parenttype::getTable();
-   }
+    static function getTypeName($nb = 0)
+    {
+        return parent::getTypeName(1);
+    }
 
-   static function getTypeName($nb = 0) {
-      return parent::getTypeName(1);
-   }
+    function isPrimaryType()
+    {
+        return true;
+    }
 
-   function isPrimaryType() {
-      return true;
-   }
-
-   function connectedTo() {
-      //Note : Interesting to have GLPI core object (who can have a tag) here
-      return [];
-   }
+    function connectedTo()
+    {
+       //Note : Interesting to have GLPI core object (who can have a tag) here
+        return [];
+    }
 
    /**
     * @see plugins/datainjection/inc/PluginDatainjectionInjectionInterface::getOptions()
    **/
-   function getOptions($primary_type = '') {
+    function getOptions($primary_type = '')
+    {
 
-      $tab = Search::getOptions(get_parent_class($this));
+        $tab = Search::getOptions(get_parent_class($this));
 
-      //Remove some options because some fields cannot be imported
-      $options['ignore_fields'] = [3, 4, 6]; //id, entity, type_menu;
-      $options['displaytype']   = ["dropdown" => [12]];
+       //Remove some options because some fields cannot be imported
+        $options['ignore_fields'] = [3, 4, 6]; //id, entity, type_menu;
+        $options['displaytype']   = ["dropdown" => [12]];
 
-      return PluginDatainjectionCommonInjectionLib::addToSearchOptions($tab, $options, $this);
-   }
+        return PluginDatainjectionCommonInjectionLib::addToSearchOptions($tab, $options, $this);
+    }
 
    /**
     * @see plugins/datainjection/inc/PluginDatainjectionInjectionInterface::addOrUpdateObject()
    **/
-   function addOrUpdateObject($values = [], $options = []) {
+    function addOrUpdateObject($values = [], $options = [])
+    {
 
-      $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
-      $lib->processAddOrUpdate();
-      $results = $lib->getInjectionResults();
+        $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
+        $lib->processAddOrUpdate();
+        $results = $lib->getInjectionResults();
 
-      // Update field for add a default value
-      if ($results['status'] == PluginDatainjectionCommonInjectionLib::SUCCESS) {
-          $item = new parent();
-          $item->update(['id'        => $results[get_parent_class()],
-                         'type_menu' => '0']); //default value
-      }
+       // Update field for add a default value
+        if ($results['status'] == PluginDatainjectionCommonInjectionLib::SUCCESS) {
+            $item = new parent();
+            $item->update(['id'        => $results[get_parent_class()],
+                'type_menu' => '0'
+            ]); //default value
+        }
 
-      return $results;
-   }
+        return $results;
+    }
 }
