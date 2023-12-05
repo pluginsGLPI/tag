84b1b7630d419f34fb1b7a5179c271017b78ef07
Cédric Anne <cedric.anne@gmail.com>
2023-12-05 16:16:45 +0100

    2.11.7

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_TAG_VERSION', '2.11.6');
+define('PLUGIN_TAG_VERSION', '2.11.7');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_TAG_MIN_GLPI", "10.0.0");
