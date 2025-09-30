2e8b00cba2d37fc260b053a8e48142c9a2cb87f3
Rom1-B <rom1.biot@gmail.com>
2025-09-30 16:24:11 +0200

    release GLPI 11.0

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_TAG_VERSION', '2.13.0-beta5');
+define('PLUGIN_TAG_VERSION', '2.13.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_TAG_MIN_GLPI", "11.0.0");
